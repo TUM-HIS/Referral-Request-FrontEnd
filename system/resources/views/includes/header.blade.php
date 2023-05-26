2a5743f9ecf9a29cd68f59c6a91447b501b85bc5
ernestnash <eernestnnashon@gmail.com>
2023-05-26 15:27:08 +0300

    Search pages UI

--- system/resources/views/includes/header.blade.php
+++ system/resources/views/includes/header.blade.php
@@ -162,7 +162,7 @@
             <li class="nav-item dropdown pe-3">
 
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                    <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                    {{-- <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle"> --}}
                     <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
                 </a><!-- End Profile Iamge Icon -->
 
