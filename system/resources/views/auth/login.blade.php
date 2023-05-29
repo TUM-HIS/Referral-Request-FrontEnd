1ea6e18160ddaa41af8ef0663188c7fa2346588d
mwaele <emmanuelmwaele@gmail.com>
2023-05-29 14:09:29 +0300

    RS-14-Modified Login page

--- system/resources/views/auth/login.blade.php
+++ system/resources/views/auth/login.blade.php
@@ -4,25 +4,19 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
-                    <div class="d-flex justify-content-center py-4">
-                        <a href="index.html" class="logo d-flex align-items-center w-auto">
-                            <img src="{{ url('assets/img/logo.png') }}" alt="">
-                            <span class="d-none d-lg-block">{{ config('app.name') }}</span>
-                        </a>
-                    </div><!-- End Logo -->
+                <img src="{{ url('assets/img/logo3.png') }}" alt="">
 
                     <div class="card mb-3">
 
                         <div class="card-body">
 
-                            <div class="pt-4 pb-2">
-                                <h6 class="card-title text-center pb-0 fs-5">Login to Your Account</h6>
-                                <p class="text-center small">Enter your username & password to login</p>
-                            </div>
-
                             <form class="row g-3 needs-validation" method="post" action="{{ route('user.login') }}">
                                 @csrf
+                                <div class="d-flex justify-content-center py-4">
+                                    <a href="index.html" class="logo d-flex align-items-center w-auto">
+                                        <span class="d-none d-lg-block">Health Referral System</span>
+                                    </a>
+                                </div><!-- End Logo -->
 
                                 <div class="col-12">
                                     <label for="yourUsername" class="form-label">Username</label>
@@ -38,13 +32,6 @@
                                     <input type="password" name="password" class="form-control" id="yourPassword" >
                                     <div class="invalid-feedback">Please enter your password!</div>
                                 </div>
-
-                                <div class="col-12">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
-                                        <label class="form-check-label" for="rememberMe">Remember me</label>
-                                    </div>
-                                </div>
                                 <div class="col-12">
                                     <button class="btn btn-primary w-100" type="submit">Login</button>
                                 </div>
@@ -55,15 +42,6 @@
 
                         </div>
                     </div>
-
-                    <div class="credits">
-                        <!-- All the links in the footer should remain intact. -->
-                        <!-- You can delete the links only if you purchased the pro version. -->
-                        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                        Designed by <a href="#">@DevKei</a>
-                    </div>
-
                 </div>
             </div>
         </div>
