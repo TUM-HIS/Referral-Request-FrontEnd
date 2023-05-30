fa6a31f4113610a1db7c1cfda8a2a3b114e8516d
mwaele <emmanuelmwaele@gmail.com>
2023-05-30 07:18:40 +0300

    RS-14 Modified login page

--- system/resources/views/auth/login.blade.php
+++ system/resources/views/auth/login.blade.php
@@ -5,12 +5,7 @@
             <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-                    <div class="d-flex justify-content-center py-4">
-                        <a href="index.html" class="logo d-flex align-items-center w-auto">
-                            <img src="{{ url('assets/img/logo.png') }}" alt="">
-                            <span class="d-none d-lg-block">{{ config('app.name') }}</span>
-                        </a>
-                    </div><!-- End Logo -->
+                <img src="{{ url('assets/img/logo.png') }}" alt="">
 
                     <div class="card mb-3">
 
