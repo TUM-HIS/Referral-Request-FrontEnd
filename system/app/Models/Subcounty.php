1c986ade33e8da929411f647b46973f856d5d098
samAricha <samaricha001.com>
2023-06-07 14:50:18 +0300

    location i.e county, sucounty models and seeding done

--- system/app/Models/Subcounty.php
+++ system/app/Models/Subcounty.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Triage extends Model
+class Subcounty extends Model
 {
     use HasFactory;
 }
