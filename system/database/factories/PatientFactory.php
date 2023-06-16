6702c6659e8111ba7da78f6050fbd481423eeb77
samAricha <samaricha001.com>
2023-06-16 11:34:21 +0300

    code clean up

--- system/database/factories/PatientFactory.php
+++ system/database/factories/PatientFactory.php
@@ -5,9 +5,7 @@ namespace Database\Factories;
 use App\Models\Patient;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
- */
+
 class PatientFactory extends Factory
 {
     protected $model = Patient::class;
