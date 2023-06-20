d99e811c4e68d9eb8090a580889bd7c651299b05
samAricha <samaricha001.com>
2023-06-20 11:35:18 +0300

    added accepted, pending and rejected referrals pie chart

--- system/resources/views/visualizations/visualizations.blade.php
+++ system/resources/views/visualizations/visualizations.blade.php
@@ -13,136 +13,12 @@
             </nav>
         </div><!-- End Page Title -->
 
-{{--        <section class="section dashboard">--}}
-{{--            <div class="row">--}}
-
-{{--                <!-- Left side columns -->--}}
-{{--                <div class="col-lg-12">--}}
-{{--                    <div class="row">--}}
-
-{{--                        <!-- Sales Card -->--}}
-{{--                        <div class="col-xxl-4 col-md-6">--}}
-{{--                            <div class="card info-card sales-card">--}}
-
-{{--                                <div class="filter">--}}
-{{--                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>--}}
-{{--                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">--}}
-{{--                                        <li class="dropdown-header text-start">--}}
-{{--                                            <h6>Filter</h6>--}}
-{{--                                        </li>--}}
-
-{{--                                        <li><a class="dropdown-item" href="#">Today</a></li>--}}
-{{--                                        <li><a class="dropdown-item" href="#">This Month</a></li>--}}
-{{--                                        <li><a class="dropdown-item" href="#">This Year</a></li>--}}
-{{--                                    </ul>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="card-body">--}}
-{{--                                    <h5 class="card-title">Patients <span>| Today</span></h5>--}}
-
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">--}}
-{{--                                            <i class="bi bi-cart"></i>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="ps-3">--}}
-{{--                                            <h6>145</h6>--}}
-{{--                                            <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>--}}
-
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-{{--                            </div>--}}
-{{--                        </div><!-- End Sales Card -->--}}
-
-{{--                        <!-- Revenue Card -->--}}
-{{--                        <div class="col-xxl-4 col-md-6">--}}
-{{--                            <div class="card info-card revenue-card">--}}
-
-{{--                                <div class="filter">--}}
-{{--                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>--}}
-{{--                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">--}}
-{{--                                        <li class="dropdown-header text-start">--}}
-{{--                                            <h6>Filter</h6>--}}
-{{--                                        </li>--}}
-
-{{--                                        <li><a class="dropdown-item" href="#">Today</a></li>--}}
-{{--                                        <li><a class="dropdown-item" href="#">This Month</a></li>--}}
-{{--                                        <li><a class="dropdown-item" href="#">This Year</a></li>--}}
-{{--                                    </ul>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="card-body">--}}
-{{--                                    <h5 class="card-title"> <span>| This Month</span></h5>--}}
-
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">--}}
-{{--                                            <i class="bi bi-currency-dollar"></i>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="ps-3">--}}
-{{--                                            <h6>$3,264</h6>--}}
-{{--                                            <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>--}}
-
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-
-{{--                            </div>--}}
-{{--                        </div><!-- End Revenue Card -->--}}
-
-{{--                        <!-- Customers Card -->--}}
-{{--                        <div class="col-xxl-4 col-xl-12">--}}
-
-{{--                            <div class="card info-card customers-card">--}}
-
-{{--                                <div class="filter">--}}
-{{--                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>--}}
-{{--                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">--}}
-{{--                                        <li class="dropdown-header text-start">--}}
-{{--                                            <h6>Filter</h6>--}}
-{{--                                        </li>--}}
-
-{{--                                        <li><a class="dropdown-item" href="#">Today</a></li>--}}
-{{--                                        <li><a class="dropdown-item" href="#">This Month</a></li>--}}
-{{--                                        <li><a class="dropdown-item" href="#">This Year</a></li>--}}
-{{--                                    </ul>--}}
-{{--                                </div>--}}
-
-{{--                                <div class="card-body">--}}
-{{--                                    <h5 class="card-title">Customers <span>| This Year</span></h5>--}}
-
-{{--                                    <div class="d-flex align-items-center">--}}
-{{--                                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">--}}
-{{--                                            <i class="bi bi-people"></i>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="ps-3">--}}
-{{--                                            <h6>1244</h6>--}}
-{{--                                            <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>--}}
-
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-
-{{--                                </div>--}}
-{{--                            </div>--}}
-
-{{--                        </div><!-- End Customers Card -->--}}
-
-                        <!-- Reports -->
+        <section class="section dashboard">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
                         <div class="col-12">
                             <div class="card">
-
-{{--                                <div class="filter">--}}
-{{--                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>--}}
-{{--                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">--}}
-{{--                                        <li class="dropdown-header text-start">--}}
-{{--                                            <h6>Filter</h6>--}}
-{{--                                        </li>--}}
-
-{{--                                        <li><a class="dropdown-item" href="#">Today</a></li>--}}
-{{--                                        <li><a class="dropdown-item" href="#">This Month</a></li>--}}
-{{--                                        <li><a class="dropdown-item" href="#">This Year</a></li>--}}
-{{--                                    </ul>--}}
-{{--                                </div>--}}
 
                                 <div class="card-body">
                                     <h5 class="card-title">Referrals <span>/per day</span></h5>
@@ -153,6 +29,12 @@
 
                                 </div>
 
+                            </div>
+                        </div>
+
+                        <br><br><br>
+                        <div class="col-6">
+                            <div class="card">
                                 <div class="card-body">
                                     <h5 class="card-title">Completed vs Incomplete Referrals <span>/Past Month</span></h5>
 
@@ -164,10 +46,24 @@
 
                             </div>
                         </div>
-                        <!-- End Reports -->
+
+                        <div class="col-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Completed vs Incomplete Referrals <span>/Past Month</span></h5>
+
+                                    <div class="width: 50%">
+                                        {!! $completedPieChart2->container() !!}
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
 
                     </div>
-                </div><!-- End Left side columns -->
+                </div>
             </div>
         </section>
     </main>
@@ -176,6 +72,7 @@
 {{--    @if($chart)--}}
         {!! $chart->script() !!}
         {!! $completedPieChart->script() !!}
+        {!! $completedPieChart2->script() !!}
 {{--    @endif--}}
 
 @endsection
