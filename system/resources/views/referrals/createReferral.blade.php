68eb1b068dcf2595783db535ca9dc0d8375554cb
mwaele <emmanuelmwaele@gmail.com>
2023-06-20 11:47:10 +0300

    Modified CreateReferral blade

--- system/resources/views/referrals/createReferral.blade.php
+++ system/resources/views/referrals/createReferral.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.backend')
 
-//adding select cdn for column selection
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js"></script>
 {{--<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>--}}
