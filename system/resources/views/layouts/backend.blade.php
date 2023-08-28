603adde494401b9899eea0d07556f656a3f4dda7
samAricha <samaricha001@gmail.com>
2023-08-28 14:57:27 +0300

    updating css resources fetching from backend.blade.php

--- system/resources/views/layouts/backend.blade.php
+++ system/resources/views/layouts/backend.blade.php
@@ -20,18 +20,19 @@
 
 
     <!-- Vendor CSS Files -->
-    <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="{{ url('assets/css/style.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/css/newPatient.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/css/outgoingTable.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/newPatient.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/outgoingTable.css') }}" rel="stylesheet">
+
 
     <link rel="stylesheet" type="text/css"href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
